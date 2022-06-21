a24aa80e62f49aab5ee16851d2d8016d0eb469c8
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-06-21 15:25:13 -0400

    complete login functionality with request helper

--- src/main/java/com/revature/web/FrontController.java
+++ src/main/java/com/revature/web/FrontController.java
@@ -10,29 +10,44 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class FrontController
  */
 public class FrontController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FrontController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * This method will be responsible for determining what resource the client is requesting
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// 1. URI rewriting
+		// http://localhost:8080/employee-servlet-app/login -- we want to capture login
+		// http://localhost:8080/employee-servlet-app/employees -- if they go here it returns all employees in the DB
+		final String URI = request.getRequestURI().replace("/employee-servlet-app/", "");
+		// we're capturing the very last part of the URI
+		
+		// set up a switch case statement in which we call the appropriate functionality based on the URI returned
+		switch(URI) {
+		
+		case "login":
+			
+			// invoke some function from the RequestHelper
+			RequestHelper.processLogin(request, response);
+			break;
+			
+		case "employees":
+			
+			// invoke some functionality from the request helper which would return all employees
+			break;
+			
+		default:
+			// custom error page
+			break;
+		}
+		
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
