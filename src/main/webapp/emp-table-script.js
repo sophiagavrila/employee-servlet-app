3bc5538d92608b9b64390c50c5d732d52a0fd423
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-06-23 12:14:24 -0400

    add pokedex functionality

--- src/main/webapp/emp-table-script.js
+++ src/main/webapp/emp-table-script.js
@@ -21,12 +21,11 @@ function buildTable(data) {
     console.log('buildTable method triggered');
 
     console.log(data);
+
     let header = document.createElement('thead'); // these are HTML elements
     let headerRow = document.createElement('tr');
 
     header.appendChild(headerRow);
-
-    // append the header to the table
     table.appendChild(header); 
 
     // create a header column for FirstName
@@ -46,6 +45,30 @@ function buildTable(data) {
     headerRow.appendChild(th2);
     headerRow.appendChild(th3);
 
+    data.forEach(e => {
+
+        console.log(e);
+
+        let row = document.createElement('tr');
+        let td1 = document.createElement('td');
+        let td2 = document.createElement('td');
+        let td3 = document.createElement('td');
+
+        // set the inner HTML of each cell to the diff propertie s (firstname, lastname, usewrnam )
+        td1.innerHTML = e.firstName;
+        td2.innerHTML = e.lastName;
+        td3.innerHTML = e.username;
+
+        // finally append each table cell to the row
+        row.appendChild(td1);
+        row.appendChild(td2);
+        row.appendChild(td3);
+
+        // append the row to table
+        table.appendChild(row);
+
+    });
+
 }
 
 function fetchEmps() {
@@ -59,7 +82,10 @@ function fetchEmps() {
     fetch(`http://${hostname}:8080/employee-servlet-app/employees`)
     .then(response => response.json()) // trakes a json string and transforms
                                         // it to a javaScript object
-    .then(obj => console.log(obj)); // print the JS obj to the console
+    //.then(obj => console.log(obj)); // print the JS obj to the console
+    .then(data => buildTable(data)); // this automatically passes the data that's been parsed
+                      // The JS object is an array of Employee objects
+                      // passes to the build table
 
 }
 
