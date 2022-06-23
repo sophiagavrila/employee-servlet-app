7d908b8bf7dc8fce3264c2b172d5e6ceca2d8801
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-06-23 11:19:14 -0400

    fix cors issue

--- src/main/java/com/revature/web/RequestHelper.java
+++ src/main/java/com/revature/web/RequestHelper.java
@@ -33,6 +33,7 @@ public class RequestHelper {
 		
 		// 1. set the content type to be application/json
 		response.setContentType("application/json");
+		response.addHeader("Access-Control-Allow-Origin", "http://localhost:8080");
 //		response.setContentType("text/html");
 		
 		// 2. Call the getAll() method from the employee service
