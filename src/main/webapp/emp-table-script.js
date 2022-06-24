a50f03f11adf65ee8c5871539fc3863dcb9f9195
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-06-24 08:28:58 -0400

    Update emp-table-script.js

--- src/main/webapp/emp-table-script.js
+++ src/main/webapp/emp-table-script.js
@@ -83,10 +83,9 @@ function fetchEmps() {
     // this is a template literal (introduced in ES6)
 
     // the following request will work for testing on localhost
-    // fetch(`http://${hostname}:8080/employee-servlet-app/employees`)
-
+    
+    fetch(`http://${hostname}:8080/employee-servlet-app/employees`)
     // this is changed because the port will be inferred when deployed on Elastic beanstalk
-    fetch(`http://${hostname}/employee-servlet-app/employees`) 
     .then(response => response.json()) // trakes a json string and transforms
                                         // it to a javaScript object
     //.then(obj => console.log(obj)); // print the JS obj to the console
